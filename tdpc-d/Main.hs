e748e415139135b45692cf7e59b039b103d04e40
ARATA Mizuki <minorinoki@gmail.com>
2019-02-06 21:32:23 +0900

    TDPC-D: Fast solution

--- tdpc-d/Main.hs
+++ tdpc-d/Main.hs
@@ -6,23 +6,23 @@ import qualified Data.Map.Strict as M
 
 type Map = M.Map Int64 Double
 
-step :: Int64 -> Map -> Map
-step !d !m = M.fromListWith (+)
-             [ (k', v')
-             | i <- [1..6]
-             , (k,v) <- M.toList m
-             , let !k' = i * k `rem` d
-                   !v' = v / 6
-             ]
+step :: Map -> Map
+step !m = M.fromListWith (+)
+          [ (d', v')
+          | i <- [1..6]
+          , (d,v) <- M.toList m
+          , let !d' = d `quot` gcd i d
+                !v' = v / 6
+          ]
 
 main = do
   l <- getLine
-  let [(n,l')] = reads l :: [(Int,String)]
-      [(d,_)] = reads l' :: [(Int64,String)]
+  let [(n,l')] = reads l :: [(Int,String)] -- 1 <= n <= 100
+      [(d,_)] = reads l' :: [(Int64,String)] -- 1 <= d <= 10^18
   if d == 1
     then print 1.0
     else do
-    let m = iterate (step d) (M.singleton 1 1.0) !! n
-    case M.lookup 0 m of
+    let m = iterate step (M.singleton d 1.0) !! n
+    case M.lookup 1 m of
       Just p -> print p
       Nothing -> print 0.0
