8ff0e0d2cf8597c5046e1133501e36cae0e6d1dc
ARATA Mizuki <minorinoki@gmail.com>
2019-02-08 16:57:30 +0900

    TDPC-F: Better solution

--- tdpc-f/Main.hs
+++ tdpc-f/Main.hs
@@ -1,12 +1,15 @@
+{-# LANGUAGE BangPatterns #-}
 import Control.Monad
 import Data.Int
 import Data.List
 import Data.Array.IO
+import qualified Data.Vector.Unboxed.Mutable as VM
 
 modulo :: Int64
 modulo = 10^9 + 7
 addMod x y = (x + y) `rem` modulo
 subMod x y = (x - y) `mod` modulo
+sumMod :: [Int64] -> Int64
 sumMod = foldl' addMod 0
 
 parseInts :: String -> [Int]
@@ -17,14 +20,15 @@ parseInts s = case reads s of
 main = do
   [n,k] <- parseInts <$> getLine
   -- 2 <= k <= n <= 10^6
-  arr <- newArray ((0, 0), (n - 1, k - 2)) 0 :: IO (IOUArray (Int,Int) Int64)
-  forM_ [0..k-2] $ \j -> do
-    writeArray arr (0, j) 1
-  forM_ [1..n-1] $ \i -> do
-    s <- sumMod <$> sequence [readArray arr (i', 0) | i' <- [0..i-2]]
-    forM_ [0..k-3] $ \j -> do
-      t <- readArray arr (i-1, j+1)
-      writeArray arr (i, j) (s `addMod` t)
-    writeArray arr (i, k-2) s
-  ans <- readArray arr (n-1, 0)
+  v <- VM.new n :: IO (VM.IOVector Int64)
+  -- t = T[i-1]
+  let loop :: Int -> Int64 -> IO Int64
+      loop !i !t | i == n = return t
+                 | otherwise = do
+                     t'' <- sumMod <$> sequence [VM.read v j | j <- [max 0 (i-k+1)..i-1]]
+                     let t' = if i < k-1 then t'' + 1 else t''
+                     s <- VM.read v (i-1)
+                     VM.write v i (s `addMod` t)
+                     loop (i+1) t'
+  ans <- loop 1 1
   print ans
