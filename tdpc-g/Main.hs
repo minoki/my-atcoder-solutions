472d898c6f92cf4252acdc57f6909bf741720b81
ARATA Mizuki <minorinoki@gmail.com>
2019-02-09 00:09:44 +0900

    Better solution for TDPC-G

--- tdpc-g/Main.hs
+++ tdpc-g/Main.hs
@@ -1,54 +1,60 @@
 {-# LANGUAGE BangPatterns #-}
 import Control.Monad
 import Data.Int
-import qualified Data.Set as S
---import Debug.Trace
 import Data.List
+import Data.Char
+import Data.Bits
 
-traceShow _ x = x
+newtype AlphabetSet = AlphabetSet Word deriving (Eq)
 
-pairs :: S.Set String -> [(Char, S.Set String)]
-pairs set = case S.minView set of
-              Just ("", set') -> pairs set'
-              Just (a:as, set') -> let (xs, ys) = S.spanAntitone (\(x:xs) -> x == a) set'
-                                   in (a, S.insert as (S.map tail xs)) : pairs ys
-              Nothing -> []
+emptyAS :: AlphabetSet
+emptyAS = AlphabetSet 0
+elemAS :: Char -> AlphabetSet -> Bool
+elemAS !x (AlphabetSet s) = testBit s (ord x - ord 'a')
+insertAS :: Char -> AlphabetSet -> AlphabetSet
+insertAS !x (AlphabetSet s) = AlphabetSet (setBit s (ord x - ord 'a'))
+fromStringAS :: String -> AlphabetSet
+fromStringAS = foldl' (flip insertAS) emptyAS
+toStringAS (AlphabetSet s) = loop s 'a'
+  where loop 0 !c = ""
+        loop s !c | testBit s 0 = c : loop (s `shiftR` 1) (succ c)
+                  | otherwise = loop (s `shiftR` 1) (succ c)
+instance Show AlphabetSet where
+  show = show . toStringAS
 
--- f (S.fromList (tails s)) : the number of substrings of s, including the empty string
-numberOfSubstrings :: S.Set String -> Int64
-numberOfSubstrings set
-  | S.null set = 0
-  | otherwise = 1 + sum [ numberOfSubstrings xss
-                        | (x,xs) <- pairs set
-                                    {-, traceShow (x,xs) True -}
-                        , let xss = S.fromList $ concatMap tails $ S.toList xs
-                        ]
+allOccurrencesNotIn :: String -> AlphabetSet -> [(Char, String)]
+allOccurrencesNotIn [] _ = []
+allOccurrencesNotIn (x:xs) e | x `elemAS` e = allOccurrencesNotIn xs e
+                             | otherwise = (x,xs) : allOccurrencesNotIn xs e
 
--- numberOfSubstrings (S.fromList (tails "aba")) == 7
--- numberOfSubstrings (S.fromList (tails "eel")) == 6
+numberOfSubstringsX :: AlphabetSet -> String -> Int64
+numberOfSubstringsX !e [] = 1
+numberOfSubstringsX !e (x:xs) | x `elemAS` e = numberOfSubstringsX e xs
+                              | otherwise = numberOfSubstringsX emptyAS xs + numberOfSubstringsX (insertAS x e) xs
 
--- set: closed w.r.t. tail (for all x:xs in set, xs is also in set)
-lexIndexOf :: Int64 -> S.Set String -> Maybe String
-lexIndexOf 0 set | S.null set = Nothing
-                 | otherwise = Just ""
-lexIndexOf !i set = case S.minView set of
-             Just ("", set') -> lexIndexOf i set'
-             Just (a:as, set') -> let (xs, ys) = S.spanAntitone (\(x:_) -> x == a) set'
-                                      set'' = S.insert as (S.map tail xs)
-                                      xss = S.fromList $ concatMap tails $ S.toList set''
-                                      m = numberOfSubstrings xss
-                                  in traceShow (i,a,set'',m) $ if i <= m
-                                                       then case lexIndexOf (i-1) xss of
-                                                              Just zs -> Just (a : zs)
-                                                              Nothing -> error "bug"
-                                                       else lexIndexOf (i - m) ys
-             Nothing -> Nothing
+lexIndexX :: Int64 -> AlphabetSet -> String -> Maybe String
+lexIndexX 0 !e !s = Just ""
+lexIndexX !i !e !s = case allOccurrencesNotIn s e of
+                       [] -> Nothing
+                       t@(_:_) -> let (x,xs) = minimumBy (\x y -> compare (fst x) (fst y)) t
+                                      n = numberOfSubstringsX emptyAS xs
+                                  in if i <= n
+                                     then case lexIndexX (i - 1) emptyAS xs of
+                                            Just t -> Just (x:t)
+                                            Nothing -> error "impossible"
+                                     else lexIndexX (i - n) (insertAS x e) s
 
--- lexIndexOf 100 (S.fromList $ tails "lexicographical") == Just "capal"
+-- lexIndexX 0 emptyAS "aba" = Just ""
+-- lexIndexX 1 emptyAS "aba" = Just "a"
+-- lexIndexX 2 emptyAS "aba" = Just "aa"
+-- lexIndexX 3 emptyAS "aba" = Just "ab"
+-- lexIndexX 4 emptyAS "aba" = Just "aba"
+-- lexIndexX 5 emptyAS "aba" = Just "b"
+-- lexIndexX 6 emptyAS "aba" = Just "ba"
 
 main = do
   s <- getLine -- 1 <= length s <= 10^6
   k <- readLn :: IO Int64 -- 1 <= k <= 10^18
-  case lexIndexOf k $ S.fromList $ tails s of
+  case lexIndexX k emptyAS s of
     Nothing -> putStrLn "Eel"
     Just t -> putStrLn t
