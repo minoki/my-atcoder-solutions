aa721bf799eba4b0e08350da105a7661e8cb3127
ARATA Mizuki <minorinoki@gmail.com>
2019-02-09 17:08:57 +0900

    TDPC-G: A better solution

--- tdpc-g/Main.hs
+++ tdpc-g/Main.hs
@@ -2,11 +2,11 @@
 import Control.Monad
 import Control.Monad.Reader
 import Data.Int
+import Data.Word
 import Data.List
 import Data.Char
 import Data.Bits
-import qualified Data.IntMap.Strict as M
-import qualified Data.Vector.Mutable as V
+import qualified Data.Vector.Unboxed.Mutable as V
 import Debug.Trace
 
 newtype AlphabetSet = AlphabetSet Int deriving (Eq)
@@ -29,54 +29,67 @@ instance Show AlphabetSet where
 allOccurrencesNotIn :: Int -> String -> AlphabetSet -> [(Char, Int, String)]
 allOccurrencesNotIn !i [] _ = []
 allOccurrencesNotIn !i (x:xs) e | x `elemAS` e = allOccurrencesNotIn (i+1) xs e
-                                | otherwise = (x,i+1,xs) : allOccurrencesNotIn (i+1) xs e
+                                | otherwise = (x,i+1,xs) : allOccurrencesNotIn (i+1) xs (insertAS x e)
 
-type Memo a = ReaderT (V.IOVector (M.IntMap Integer)) IO a
+type INT = {- Integer -} Word64
 
-numberOfSubstringsX :: AlphabetSet -> Int -> String -> Memo Integer
-numberOfSubstringsX !e !i [] = return 1
-numberOfSubstringsX !e@(AlphabetSet ex) !i s = do
+satAdd :: INT -> INT -> INT
+satAdd !a !b = let !c = a + b
+               in if c < a || c < b
+                  then maxBound
+                  else c
+
+type Memo a = ReaderT (V.IOVector INT) IO a
+
+numberOfSubstrings :: Int -> String -> Memo INT
+numberOfSubstrings !i [] = return 1
+numberOfSubstrings !i s = do
   arr <- ask
   m <- V.read arr i
-  case M.lookup ex m of
-    Just v -> return v
-    Nothing -> do v <- loop e i s
-                  -- if v < 0 then traceShow (e,i,s,v) $ error "numberOfSubstringsX: impossible" else return ()
-                  V.write arr i (M.insert ex v m)
-                  return v
-  where
-    loop !e !i (x:xs) | x `elemAS` e = numberOfSubstringsX e (i+1) xs
-                      | otherwise = liftM2 (+) (numberOfSubstringsX emptyAS (i+1) xs) (numberOfSubstringsX (insertAS x e) (i+1) xs)
+  if m /= 0
+    then return m
+    else do v <- doCalc i s
+            V.write arr i v
+            return v
+              where -- doCalc i s = foldl' satAdd 0 <$> sequence [ numberOfSubstrings j xs | (x,j,xs) <- allOccurrencesNotIn i s emptyAS ]
+                    doCalc i s = loop 1 (allOccurrencesNotIn i s emptyAS)
+                    loop !acc [] = return acc
+                    loop !acc _ | acc == maxBound = return acc
+                    loop !acc ((x,j,xs):xss) = do y <- numberOfSubstrings j xs
+                                                  loop (satAdd acc y) xss
 
-lexIndexX :: Integer -> AlphabetSet -> Int -> String -> Memo (Maybe String)
-lexIndexX 0 !e !j !s = return (Just "")
-lexIndexX !i !e !j !s
-  | i > 0 = case allOccurrencesNotIn j s e of
-              [] -> return Nothing
-              t@(_:_) -> do
-                let (x,j',xs) = minimumBy (\(x,_,_) (y,_,_) -> compare x y) t
-                n <- numberOfSubstringsX emptyAS j' xs
-                if i <= n
-                  then do v <- lexIndexX (i - 1) emptyAS j' xs
-                          case v of
-                            Just t -> return (Just (x:t))
-                            Nothing -> traceShow (i,x,xs) $ error "impossible"
-                  else lexIndexX (i - n) (insertAS x e) j s
-  | otherwise = traceShow (i,e,j,s) $ error "impossible"
+lexIndexW :: INT -> [(Char, Int, String)] -> Memo (Maybe String)
+lexIndexW 0 !_ = return (Just "")
+lexIndexW !i [] = return Nothing
+lexIndexW !i ((x,j,xs):xss) = do
+  n <- numberOfSubstrings j xs
+  if i <= n
+    then do v <- lexIndexX (i - 1) j xs
+            case v of
+              Just t -> return (Just (x:t))
+              Nothing -> traceShow (i,x,xs) $ error "impossible"
+    else lexIndexW (i - n) xss
 
--- lexIndexX 0 emptyAS "aba" = Just ""
--- lexIndexX 1 emptyAS "aba" = Just "a"
--- lexIndexX 2 emptyAS "aba" = Just "aa"
--- lexIndexX 3 emptyAS "aba" = Just "ab"
--- lexIndexX 4 emptyAS "aba" = Just "aba"
--- lexIndexX 5 emptyAS "aba" = Just "b"
--- lexIndexX 6 emptyAS "aba" = Just "ba"
+lexIndexX :: INT -> Int -> String -> Memo (Maybe String)
+lexIndexX i j s = lexIndexW i $ sortBy (\(x,_,_) (y,_,_) -> compare x y) $ allOccurrencesNotIn j s emptyAS
+
+-- lexIndexX 0 "aba" = Just ""
+-- lexIndexX 1 "aba" = Just "a"
+-- lexIndexX 2 "aba" = Just "aa"
+-- lexIndexX 3 "aba" = Just "ab"
+-- lexIndexX 4 "aba" = Just "aba"
+-- lexIndexX 5 "aba" = Just "b"
+-- lexIndexX 6 "aba" = Just "ba"
+
+runMemo :: String -> Memo a -> IO a
+runMemo s m = do
+  arr <- V.new (length s + 1)
+  runReaderT m arr
 
 main = do
   s <- getLine -- 1 <= length s <= 10^6
-  k <- readLn :: IO Integer -- 1 <= k <= 10^18
-  arr <- V.replicate (length s + 1) M.empty
-  v <- runReaderT (lexIndexX k emptyAS 0 s) arr
+  k <- readLn :: IO INT -- 1 <= k <= 10^18
+  v <- runMemo s (lexIndexX k 0 s)
   case v of
     Nothing -> putStrLn "Eel"
     Just t -> putStrLn t
