27ec73ca5dcbca34163dbe9d7f49bb4c4e2a78a2
ARATA Mizuki <minorinoki@gmail.com>
2019-02-09 17:46:27 +0900

    TDPC-G: Improve C++ version

--- tdpc-g/main.cpp
+++ tdpc-g/main.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
 #include <vector>
-#include <map>
 #include <cstdint>
 #include <utility>
 #include <string>
 #include <algorithm>
 #include <limits>
 
-using Integer = std::uint64_t;
+using INT = std::uint64_t;
 
 struct AlphabetSet {
     std::int32_t val;
@@ -36,61 +35,78 @@ auto allOccurrenceNotIn(const char *s, AlphabetSet e) -> std::vector<std::pair<c
 }
 
 const char *strBase;
-std::vector<std::map<int, Integer>> memo;
+std::vector<INT> memo;
 
-auto numberOfSubstringsX(AlphabetSet const e, const char *s) -> Integer
-{
-    if (*s == '\0') {
-        return 1;
-    }
-    auto& memoMap = memo.at(s - strBase);
-    if (memoMap.count(e.val) > 0) {
-        return memoMap[e.val];
-    }
-    Integer val = 0;
-    if (e.elem(*s)) {
-        val = numberOfSubstringsX(e, s+1);
-    } else {
-        auto a = numberOfSubstringsX(AlphabetSet::empty, s+1);
-        auto b = numberOfSubstringsX(e.insert(*s), s+1);
-        auto c = a + b;
-        if (c < a || c < b) {
-            val = std::numeric_limits<std::uint64_t>::max();
-        } else {
-            val = c;
-        }
-    }
-    memoMap[e.val] = val;
-    return val;
-}
+using Pair = std::pair<char, const char *>;
 
-auto lexIndexX(Integer i, AlphabetSet const e, const char *s) -> std::string
+auto lexIndexX(INT i, const char *s) -> std::string;
+
+auto lexIndexW(INT i, std::vector<Pair>::const_iterator it, std::vector<Pair>::const_iterator end, INT maxI) -> std::string
 {
-    if (i == 0) {
-        return "";
-    } else {
-        auto const& t = allOccurrenceNotIn(s, e);
-        if (t.empty()) {
+    for (;;) {
+        if (i == 0) {
+            return "";
+        } else if (it == end) {
             return "Eel";
         } else {
-            auto const& p = *std::min_element(t.begin(), t.end(), [](std::pair<char, const char *> const& a, std::pair<char, const char *> const& b) { return a.first < b.first; });
-            auto n = numberOfSubstringsX(AlphabetSet::empty, p.second);
+            auto n = memo.at(it->second - strBase);
             if (i <= n) {
-                return p.first + lexIndexX(i - 1, AlphabetSet::empty, p.second);
+                return it->first + lexIndexX(i - 1, it->second);
             } else {
-                return lexIndexX(i - n, e.insert(p.first), s);
+                // return lexIndexW(i - n, ++it, end, maxI);
+                i -= n;
+                ++it;
             }
         }
     }
 }
 
+auto lexIndexX(INT i, const char *s) -> std::string
+{
+    auto t = allOccurrenceNotIn(s, AlphabetSet::empty);
+    std::sort(t.begin(), t.end(), [](Pair const& a, Pair const& b) { return a.first < b.first; });
+    return lexIndexW(i, t.begin(), t.end(), i);
+}
+
 int main()
 {
     std::string str;
-    std::getline(std::cin, str);
-    Integer k = 0;
-    std::cin >> k;
-    memo.resize(str.size() + 1);
+    // std::ifstream ins("testinput.txt");
+    auto& ins = std::cin;
+    std::getline(ins, str);
+    INT k = 0;
+    ins >> k;
     strBase = str.c_str();
-    std::cout << lexIndexX(k, AlphabetSet::empty, str.c_str()) << std::endl;
+    {
+        memo.resize(str.size() + 1);
+        for (const char *s = strBase + str.size(); /* s >= strBase */; --s) {
+            INT val = 1;
+            {
+                AlphabetSet e = AlphabetSet::empty;
+                for (const char *t = s; *t; ++t) {
+                    if (!e.elem(*t)) {
+                        val += memo.at(t+1 - strBase);
+                        if (val > k) {
+                            break;
+                        }
+                        e = e.insert(*t);
+                    }
+                }
+            }
+            /*
+              auto const& t = allOccurrenceNotIn(s, AlphabetSet::empty);
+              for (auto const& u : t) {
+              val += memo.at(u.second - strBase);
+              if (val > k) {
+              break;
+              }
+              }
+            */
+            memo.at(s - strBase) = val;
+            if (s == strBase) {
+                break;
+            }
+        }
+    }
+    std::cout << lexIndexX(k, str.c_str()) << std::endl;
 }
