b0d2dcac484a85522b9264118c69d5a840c53482
ARATA Mizuki <minorinoki@gmail.com>
2019-02-10 17:18:39 +0900

    TDPC-G: Add a script to generate non-random input

---
+++ tdpc-g/mklargeinput.lua
@@ -0,0 +1,12 @@
+math.randomseed(os.time())
+
+local a = string.byte "a"
+local t = {}
+for i = 1, 10^6 do
+  t[i] = "q" -- string.char(a + math.floor(math.random() * 26))
+end
+local f = assert(io.open("testinput2.txt", "w"))
+f:write(table.concat(t), "\n")
+f:write("1000000000000000000\n")
+f:close()
+
