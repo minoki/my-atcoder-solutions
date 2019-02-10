26110d119369000ab830655b5f9e8a819934d44b
ARATA Mizuki <minorinoki@gmail.com>
2019-02-10 17:48:43 +0900

    TDPC-G: Fast solution (C++)

--- tdpc-g/main.cpp
+++ tdpc-g/main.cpp
@@ -5,53 +5,84 @@
 #include <string>
 #include <algorithm>
 #include <limits>
+#include <array>
 
-using INT = std::uint64_t;
+std::string str;
+std::vector<std::array<std::int32_t, 26>> strIndexTable;
 
-struct AlphabetSet {
-    std::int32_t val;
-    static const AlphabetSet empty;
-    static const AlphabetSet all;
-    constexpr auto elem(char c) const -> bool
-    {
-        return !!(val & (1 << (c - 'a')));
-    }
-    constexpr auto insert(char c) const -> AlphabetSet
-    {
-        return {val | (1 << (c - 'a'))};
-    }
-    constexpr auto operator==(AlphabetSet rhs) const -> bool
-    {
-        return val == rhs.val;
-    }
-    constexpr auto operator!=(AlphabetSet rhs) const -> bool
-    {
-        return val != rhs.val;
-    }
-};
-const AlphabetSet AlphabetSet::empty = {0};
-const AlphabetSet AlphabetSet::all = {(1 << 26) - 1};
-
-auto allOccurrencesNotIn(const char *s, AlphabetSet e) -> std::vector<std::pair<char, const char *>>
+// strIndexTable[i][x - 'a'] : 文字列の i 番目以降に最初に現れる文字 x のインデックス（現れない場合は -1）
+// となるような2次元配列 strIndexTable を初期化する。
+void initStringIndexTable()
 {
-    std::vector<std::pair<char, const char *>> res;
-    for (; *s && e != AlphabetSet::all; ++s) {
-        if (!e.elem(*s)) {
-            res.emplace_back(*s, s+1);
-            e = e.insert(*s);
+    strIndexTable.resize(str.size() + 1);
+    for(std::size_t j = 0; j < 26; ++j) {
+        strIndexTable.back()[j] = -1;
+    }
+    for(std::size_t i = str.size() - 1; ; --i) {
+        strIndexTable.at(i) = strIndexTable.at(i + 1);
+        char x = str.at(i);
+        strIndexTable.at(i).at(x - 'a') = static_cast<std::int32_t>(i);
+        if (i == 0) {
+            break;
+        }
+    }
+}
+
+using Pair = std::pair<char, int>;
+
+// 文字列の i 番目以降に出現する文字と、その最初の出現位置の組 Pair(c, j) のリストを返す。
+// リストの順番は、アルファベットの若い順である。
+auto allOccurrences(int i) -> std::vector<Pair>
+{
+    std::vector<Pair> res;
+    auto const& tbl = strIndexTable.at(i);
+    if (i < str.size()) {
+        for (int k = 0; k < 26; ++k) {
+            if (tbl.at(k) != -1) {
+                res.emplace_back('a' + k, tbl.at(k));
+            }
         }
     }
     return res;
 }
 
-const char *strBase;
-std::vector<INT> memo;
+std::vector<std::int64_t> numberOfSubstringsVec;
 
-using Pair = std::pair<char, const char *>;
+// numberOfSubstringsVec[i] = 〈文字列の i 番目以降からなるスライス〉の部分文字列の個数
+// となるような配列 numberOfSubstringsVec を初期化する。
+// ただし、値が十分大きい場合の計算は適宜打ち切られる（十分大きい値が適当に入っている）。
+void initNumberOfSubstringsVec()
+{
+    numberOfSubstringsVec.resize(str.size() + 1);
+    const std::int64_t maxI = 1e18 + 1; // 十分大きな値
+    for (std::size_t i = str.size(); ; --i) {
+        std::int64_t acc = 1;
+        for (auto const& p : allOccurrences(i)) {
+            if (acc > maxI) {
+                // 値が十分大きくなった場合は計算を打ち切ってオーバーフローを回避する
+                break;
+            }
+            auto y = numberOfSubstringsVec.at(p.second + 1);
+            if (y > maxI) {
+                // 値が十分大きくなった場合はry
+                acc = y;
+                break;
+            }
+            acc += y;
+        }
+        numberOfSubstringsVec.at(i) = acc;
+        if (i == 0) {
+            break;
+        }
+    }
+}
 
-auto lexIndexX(INT i, const char *s) -> std::string;
+auto lexIndexSlice(int j, std::int64_t i) -> std::string;
 
-auto lexIndexW(INT i, std::vector<Pair>::const_iterator it, std::vector<Pair>::const_iterator end, INT maxI) -> std::string
+// 与えられた集合 S の要素の部分文字列のうち、辞書順で i 番目となるものを返す。
+// S は、文脈で与えられる文字列に対して (最初の一文字, その出現位置) という形の要素からなるリストで、アルファベットの若い順に並んでいる。
+// i が大きいときは Nothing を返す。
+auto lexSearch(std::int64_t i, std::vector<Pair>::const_iterator it, std::vector<Pair>::const_iterator end) -> std::string
 {
     for (;;) {
         if (i == 0) {
@@ -59,11 +90,11 @@ auto lexIndexW(INT i, std::vector<Pair>::const_iterator it, std::vector<Pair>::c
         } else if (it == end) {
             return "Eel";
         } else {
-            auto n = memo.at(it->second - strBase);
+            auto n = numberOfSubstringsVec.at(it->second + 1);
             if (i <= n) {
-                return it->first + lexIndexX(i - 1, it->second);
+                return it->first + lexIndexSlice(it->second + 1, i - 1);
             } else {
-                // return lexIndexW(i - n, ++it, end, maxI);
+                // return lexSearch(i - n, ++it, end);
                 i -= n;
                 ++it;
             }
@@ -71,52 +102,20 @@ auto lexIndexW(INT i, std::vector<Pair>::const_iterator it, std::vector<Pair>::c
     }
 }
 
-auto lexIndexX(INT i, const char *s) -> std::string
+// 〈文字列の j 番目以降からなるスライス〉の部分文字列のうち、辞書順で i 番目となるものを返す。
+// i が大きいときは Nothing を返す。
+auto lexIndexSlice(int j, std::int64_t i) -> std::string
 {
-    auto t = allOccurrencesNotIn(s, AlphabetSet::empty);
-    std::sort(t.begin(), t.end(), [](Pair const& a, Pair const& b) { return a.first < b.first; });
-    return lexIndexW(i, t.begin(), t.end(), i);
+    auto t = allOccurrences(j);
+    return lexSearch(i, t.begin(), t.end());
 }
 
 int main()
 {
-    std::string str;
-    // std::ifstream ins("testinput.txt");
-    auto& ins = std::cin;
-    std::getline(ins, str);
-    INT k = 0;
-    ins >> k;
-    strBase = str.c_str();
-    {
-        memo.resize(str.size() + 1);
-        for (const char *s = strBase + str.size(); /* s >= strBase */; --s) {
-            INT val = 1;
-            {
-                AlphabetSet e = AlphabetSet::empty;
-                for (const char *t = s; *t && e != AlphabetSet::all; ++t) {
-                    if (!e.elem(*t)) {
-                        val += memo.at(t+1 - strBase);
-                        if (val > k) {
-                            break;
-                        }
-                        e = e.insert(*t);
-                    }
-                }
-            }
-            /*
-              auto const& t = allOccurrencesNotIn(s, AlphabetSet::empty);
-              for (auto const& u : t) {
-              val += memo.at(u.second - strBase);
-              if (val > k) {
-              break;
-              }
-              }
-            */
-            memo.at(s - strBase) = val;
-            if (s == strBase) {
-                break;
-            }
-        }
-    }
-    std::cout << lexIndexX(k, str.c_str()) << std::endl;
+    std::getline(std::cin, str);
+    std::int64_t k = 0;
+    std::cin >> k;
+    initStringIndexTable();
+    initNumberOfSubstringsVec();
+    std::cout << lexIndexSlice(0, k) << std::endl;
 }
