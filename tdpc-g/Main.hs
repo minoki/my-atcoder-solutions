d6577902059ac22e4a9d8e3e7aa7781d5928bcef
ARATA Mizuki <minorinoki@gmail.com>
2019-02-10 15:21:32 +0900

    TDPC-G: Use ByteString

--- tdpc-g/Main.hs
+++ tdpc-g/Main.hs
@@ -7,6 +7,8 @@ import Data.Char
 import Data.Bits
 import qualified Data.Vector.Unboxed as V
 import qualified Data.Vector.Unboxed.Mutable as V
+import Data.ByteString.Char8 (ByteString)
+import qualified Data.ByteString.Char8 as BS
 
 newtype AlphabetSet = AlphabetSet Int deriving (Eq)
 
@@ -23,17 +25,17 @@ toStringAS (AlphabetSet s) = loop s 'a'
   where loop 0 !c = ""
         loop s !c | testBit s 0 = c : loop (s `shiftR` 1) (succ c)
                   | otherwise = loop (s `shiftR` 1) (succ c)
-
 instance Show AlphabetSet where
   show = show . toStringAS
 
-allOccurrencesNotIn :: Int -> String -> AlphabetSet -> [(Char, Int, String)]
-allOccurrencesNotIn !i [] !_ = []
-allOccurrencesNotIn !i (x:xs) !e
-  | e == allAS = []
-  | x `elemAS` e = allOccurrencesNotIn (i+1) xs e
-  | otherwise = let !i' = i+1
-                in (x,i',xs) : allOccurrencesNotIn i' xs (insertAS x e)
+allOccurrencesNotIn :: Int -> ByteString -> AlphabetSet -> [(Char, Int, ByteString)]
+allOccurrencesNotIn !i !s !e
+  = case BS.uncons s of
+      Nothing -> []
+      Just (x,xs) | e == allAS -> []
+                  | x `elemAS` e -> allOccurrencesNotIn (i+1) xs e
+                  | otherwise -> let !i' = i+1
+                                 in (x,i',xs) : allOccurrencesNotIn i' xs (insertAS x e)
 
 type INT = {- Integer -} Word64
 
@@ -43,11 +45,11 @@ satAdd !a !b = let !c = a + b
                   then maxBound
                   else c
 
-numberOfSubstringsV :: INT -> String -> V.Vector INT
+numberOfSubstringsV :: INT -> ByteString -> V.Vector INT
 numberOfSubstringsV !maxI s = V.create $ do
-  let ls = length s
+  let ls = BS.length s
   v <- V.new (ls + 1)
-  forM_ (reverse $ zip [0..] $ tails s) $ \(i,ss) -> do
+  forM_ (reverse $ zip [0..] $ BS.tails s) $ \(i,ss) -> do
     let loop !acc [] = return acc
         loop !acc _ | acc > maxI = return acc
         -- loop !acc _ | acc == maxBound = return acc
@@ -59,19 +61,19 @@ numberOfSubstringsV !maxI s = V.create $ do
 
 type Memo a = Reader (V.Vector INT) a
 
-lexIndexW :: INT -> [(Char, Int, String)] -> Memo (Maybe String)
-lexIndexW 0 !_ = return (Just "")
+lexIndexW :: INT -> [(Char, Int, ByteString)] -> Memo (Maybe ByteString)
+lexIndexW 0 !_ = return (Just BS.empty)
 lexIndexW !i [] = return Nothing
 lexIndexW !i ((x,j,xs):xss) = do
   n <- asks (V.! j)
   if i <= n
     then do v <- lexIndexX (i - 1) j xs
             case v of
-              Just t -> return (Just (x:t))
+              Just t -> return (Just (BS.cons x t))
               Nothing -> error "impossible"
     else lexIndexW (i - n) xss
 
-lexIndexX :: INT -> Int -> String -> Memo (Maybe String)
+lexIndexX :: INT -> Int -> ByteString -> Memo (Maybe ByteString)
 lexIndexX i j s = lexIndexW i $ sortBy (\(x,_,_) (y,_,_) -> compare x y) $ allOccurrencesNotIn j s emptyAS
 
 -- lexIndexX 0 0 "aba" = Just ""
@@ -83,10 +85,10 @@ lexIndexX i j s = lexIndexW i $ sortBy (\(x,_,_) (y,_,_) -> compare x y) $ allOc
 -- lexIndexX 6 0 "aba" = Just "ba"
 
 main = do
-  s <- getLine -- 1 <= length s <= 10^6
+  s <- BS.getLine -- 1 <= length s <= 10^6
   k <- readLn :: IO INT -- 1 <= k <= 10^18
   let arr = numberOfSubstringsV k s
       v = runReader (lexIndexX k 0 s) arr
   case v of
     Nothing -> putStrLn "Eel"
-    Just t -> putStrLn t
+    Just t -> BS.putStrLn t
