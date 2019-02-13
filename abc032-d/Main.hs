a4b9d5988e56a13569f922cc496dc820523540ae
ARATA Mizuki <minorinoki@gmail.com>
2019-02-14 02:46:10 +0900

    ABC032-D: Good solution

--- abc032-d/Main.hs
+++ abc032-d/Main.hs
@@ -2,7 +2,10 @@
 import Control.Monad
 import qualified Data.IntMap as M
 import qualified Data.Vector.Unboxed as V
+import qualified Data.Vector.Mutable as VM
+import Data.Array.IO
 import Data.Int
+import System.Environment
 
 parseInts :: String -> [Int]
 parseInts s = case reads s of
@@ -10,6 +13,7 @@ parseInts s = case reads s of
                 _ -> []
 
 -- wi's are small
+-- vec V.! w = (the maximum value obtained with the weight w)
 solveV :: Int -> [(Int,Int)] -> V.Vector Int -> V.Vector Int
 solveV !maxW [] map = map
 solveV !maxW ((!v,!w):xs) map = solveV maxW xs
@@ -31,15 +35,98 @@ solveV !maxW ((!v,!w):xs) map = solveV maxW xs
             else map
 
 -- vi's are small
+-- map M.! v = (the minimum weight needed to get the value v)
 solveM :: Int -> [(Int,Int)] -> M.IntMap Int -> M.IntMap Int
 solveM !maxW [] map = map
-solveM !maxW ((!v,!w):xs) map = solveM maxW xs
-  $ M.unionWith min map
-  $ M.fromAscList [ (v' + v, ww)
-                  | (v',w') <- M.toAscList map
-                  , let !ww = w + w'
-                  , ww <= maxW
-                  ]
+solveM !maxW ((!v,!w):xs) map
+  | w <= maxW = solveM maxW xs
+    $ M.unionWith min map
+    $ M.fromAscList [ (vv, ww)
+                    | (v',w') <- M.toAscList map
+                    , let !ww = w + w'
+                    , ww <= maxW
+                    , let !vv = v' + v
+                    ]
+  | otherwise = solveM maxW xs map
+
+solveL :: Int -> [(Int,Int)] -> [(Int,Int)] -> [(Int,Int)]
+solveL !maxW [] map = map
+solveL !maxW ((!v,!w):xs) map
+  | w <= maxW = solveL maxW xs
+    $ merge map [ (vv, ww)
+                | (v',w') <- map
+                , let !ww = w + w'
+                , ww <= maxW
+                , let !vv = v' + v
+                ]
+  | otherwise = solveL maxW xs map
+  where
+    merge [] ys = ys
+    merge xs [] = xs
+    merge xs@(x@(v,w):xss) ys@(y@(v',w'):yss)
+      = case compare v v' of
+          LT -> y : merge xs yss
+          GT -> x : merge xss ys
+          EQ -> (v, min w w') : merge xss yss
+
+solveA :: Int -> Int -> [(Int,Int)] -> IOUArray (Int,Int) Int -> IO Int
+solveA !0 !_ _ arr = return 0
+solveA !maxW !i [] arr = return 0
+solveA !maxW !i ((!v,!w):xs) arr = do
+  u <- readArray arr (i, maxW)
+  if u == -1
+    then do u' <- do a <- solveA maxW (i+1) xs arr
+                     if w > maxW
+                       then return a
+                       else do b <- solveA (maxW - w) (i+1) xs arr
+                               return $ max a (b+v)
+            writeArray arr (i, maxW) u'
+            return u'
+    else return u
+
+solveAM :: Int -> Int -> Int -> [(Int,Int)] -> VM.IOVector (M.IntMap Int) -> IO Int
+solveAM !0 !sumW !_ _ arr = return 0
+solveAM !maxW !sumW !i [] arr = return 0
+solveAM !maxW !sumW !i ((!v,!w):xs) arr = do
+  map <- VM.read arr i
+  case M.lookup maxW map of
+    Just u -> return u
+    Nothing -> do
+      u <- if sumW <= maxW
+           then return $ v + sum [vi | (vi,_) <- xs]
+           else do a <- solveAM maxW (sumW - w) (i+1) xs arr
+                   if w > maxW
+                     then return a
+                     else do b <- solveAM (maxW - w) (sumW - w) (i+1) xs arr
+                             return $ max a (b+v)
+      VM.write arr i (M.insert maxW u map)
+      return u
+
+-- vec V.! v = (the minimum weight needed to get the value v)
+solveVV :: Int -> [(Int,Int)] -> V.Vector Int -> V.Vector Int
+solveVV !maxW [] map = map
+solveVV !maxW ((!v,!w):xs) map
+  | w <= maxW = solveVV maxW xs
+    $ trim $ if v < V.length map
+             then V.generate (v + V.length map)
+                  $ \i -> if i < v
+                          then map V.! i
+                          else if i < V.length map
+                               then let ww = (map V.! (i - v)) + w
+                                    in if ww <= maxW
+                                       then min (map V.! i) ww
+                                       else map V.! i
+                               else (map V.! (i - v)) + w
+             else let l = v + V.length map
+                  in V.generate l
+                     $ \i -> if i < V.length map
+                             then map V.! i
+                             else if i < v
+                                  then maxW + 1 -- infinity
+                                  else (map V.! (i - v)) + w
+  | otherwise = solveVV maxW xs map
+  where trim v | V.last v > maxW = trim (V.init v)
+               | otherwise = v
 
 main = do
   [n,maxW] <- parseInts <$> getLine
@@ -49,8 +136,48 @@ main = do
     -- 1 <= vi <= 10^9
     -- 1 <= wi <= 10^9
     return (vi,wi)
-  if all (\(_,wi) -> wi <= 1000) xs
-    then let m = solveV maxW xs (V.singleton 0)
-         in print (V.maximum m)
-    else let m = solveM maxW xs (M.singleton 0 0)
-         in print (fst $ M.findMax m)
+  let sumW = sum [wi | (_,wi) <- xs]
+  let solve1 = do
+        -- dataset 2
+        -- wi's are small
+        -- uses vector
+        let m = solveV maxW xs (V.singleton 0)
+        print (V.maximum m)
+      solve2 = do
+        -- vi's are small
+        -- uses map
+        let m = solveM maxW xs (M.singleton 0 0)
+        print (fst $ M.findMax m)
+      solve2L = do
+        -- vi's are small
+        -- uses list
+        let m = solveL maxW xs [(0,0)]
+        print (fst $ head m)
+      solve3 = do
+        -- memoize with vector + map
+        arr <- VM.replicate (n + 1) M.empty
+        ans <- solveAM maxW sumW 0 xs arr
+        print ans
+      solve4 = do
+        -- memoize with array
+        arr <- newArray ((0, 0), (n, maxW)) (-1)
+        ans <- solveA maxW 0 xs arr
+        print ans
+      solve5 = do -- dataset 3
+        -- vi's are small
+        -- uses map
+        let m = solveVV maxW xs (V.singleton 0)
+        print (V.length m - 1)
+  args <- getArgs
+  case args of
+    "vector":_ -> solve1
+    "vectorV":_ -> solve5
+    "map":_ -> solve2
+    "list":_ -> solve2L
+    "memo":_ -> solve3
+    "memoA":_ -> solve4
+    _ -> if sumW <= maxW
+         then print $ sum [vi | (vi,_) <- xs]
+         else if maxW <= 200000 -- all (\(_,wi) -> wi <= 1000) xs
+              then solve1
+              else solve2L
--- abc032-d/mkrandinput.lua
+++ abc032-d/mkrandinput.lua
@@ -1,7 +1,7 @@
 math.randomseed(os.time())
 
 local f = assert(io.open("dataset2.txt", "w"))
-local n = 100
+local n = 200
 local maxW = math.random(1, 10^9)
 f:write(string.format("%d %d\n", n, maxW))
 for i = 1, n do
@@ -11,13 +11,40 @@ for i = 1, n do
 end
 f:close()
 
-local f = assert(io.open("dataset3.txt", "w"))
-local n = 100
+local f = assert(io.open("dataset3-1.txt", "w"))
+local n = 200
 local maxW = math.random(1, 10^9)
 f:write(string.format("%d %d\n", n, maxW))
 for i = 1, n do
   local vi = math.random(1, 1000)
   local wi = math.random(1, 10^9)
   f:write(string.format("%d %d\n", vi, wi))
+end
+f:close()
+
+local f = assert(io.open("dataset3-2.txt", "w"))
+local n = 200
+local maxW = math.random(10^9, 10^9)
+f:write(string.format("%d %d\n", n, maxW))
+for i = 1, n do
+  local vi = math.random(100, 1000)
+  local wi = math.random(1, 10^6)
+  f:write(string.format("%d %d\n", vi, wi))
+end
+f:close()
+
+local n = 200
+local t,s = {},0
+for i = 1, n do
+  local vi = math.random(1, 1000)
+  local wi = math.random(1, 10^6)
+  table.insert(t, {vi, wi})
+  s = s + wi
+end
+local maxW = math.min(10^9, math.ceil(s / 1.7))
+local f = assert(io.open("dataset3-3.txt", "w"))
+f:write(string.format("%d %d\n", n, maxW))
+for i,v in ipairs(t) do
+  f:write(string.format("%d %d\n", v[1], v[2]))
 end
 f:close()
