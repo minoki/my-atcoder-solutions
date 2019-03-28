c7063cfac1aa1ebd1e019bf1c79b44520bec19e7
ARATA Mizuki <minorinoki@gmail.com>
2019-03-29 01:41:49 +0900

    DP-C: Use foldl1'

--- dp-c/Main.hs
+++ dp-c/Main.hs
@@ -9,5 +9,5 @@ main = do
     [a,b,c] <- map (read . BS.unpack) . BS.words <$> BS.getLine
     -- a,b,c <= 10^4
     return (a,b,c :: Int)
-  let (a,b,c) = V.last $ V.scanl1' (\(a,b,c) (a',b',c') -> (max b c + a', max a c + b', max a b + c')) happiness
+  let (a,b,c) = V.foldl1' (\(a,b,c) (a',b',c') -> (max b c + a', max a c + b', max a b + c')) happiness
   print (max a (max b c))
