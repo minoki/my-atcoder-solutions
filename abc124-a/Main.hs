c6552b72db3cf86aab236a1ac1e37b9da4c77cac
ARATA Mizuki <minorinoki@gmail.com>
2019-04-13 21:40:09 +0900

    ABC124: Clean up

--- abc124-a/Main.hs
+++ abc124-a/Main.hs
@@ -1,15 +1,4 @@
-{-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-import Control.Monad
-import Data.Int (Int64)
-import Data.List (foldl')
-import Data.Array
-import Data.Array.Unboxed
-import Data.Array.ST
-import qualified Data.Vector as V
-import qualified Data.Vector.Mutable as VM
-import qualified Data.Vector.Unboxed as U
-import qualified Data.Vector.Unboxed.Mutable as UM
 import qualified Data.ByteString.Char8 as BS
 
 main = do
