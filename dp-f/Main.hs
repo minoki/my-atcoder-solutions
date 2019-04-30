c40cb8fea972359cee0811b2eb24bb5c13162bad
ARATA Mizuki <minorinoki@gmail.com>
2019-04-30 16:41:43 +0000

    DP-F: Use scanr

--- dp-f/Main.hs
+++ dp-f/Main.hs
@@ -1,51 +1,45 @@
 {-# LANGUAGE BangPatterns #-}
-import Control.Monad
 import Data.Word
-import qualified Data.ByteString as BSW
 import qualified Data.ByteString.Char8 as BS
-import Data.Array.Unboxed
-import Data.Array.ST
+import qualified Data.Vector as V
+import qualified Data.Vector.Unboxed as U
 
 -- Input: s t
 -- Output: arr
---   arr ! (i,j) == length of lcs of (drop i s, drop j t)
-lcsTable2 :: BS.ByteString -> BS.ByteString -> UArray (Int,Int) Word16
-lcsTable2 s t = runSTUArray $ do
-  let !m = BS.length s
-      !n = BS.length t
-  arr <- newArray ((0,0),(m,n)) 0
-  forM_ [m-1,m-2..0] $ \ !i -> do
-    let !x = BSW.index s i
-        loopY !j !v
-          -- v = readArray arr (i,j+1)
-          | j >= 0 = do
-              let !y = BSW.index t j
-              if x == y
-                then do l2 <- readArray arr (i+1,j+1)
-                        let !l = l2 + 1
-                        writeArray arr (i,j) l
-                        loopY (j-1) l
-                else do l0 <- readArray arr (i+1,j)
-                        let !l = max l0 v
-                        writeArray arr (i,j) l
-                        loopY (j-1) l
-          | otherwise = return ()
-    loopY (n-1) 0
-  return arr
+--   (arr ! i) ! j == length of lcs of (drop i s, drop j t)
+lcsTable :: BS.ByteString -> BS.ByteString -> V.Vector (U.Vector Word16)
+lcsTable s t = V.scanr' (\ !x !v ->
+                           -- for some i (0 <= i < m),
+                           --   x = BS.index s i :: Char
+                           --   v = arr ! (i+1) :: U.Vector Word16
+                           U.scanr (\(!y,!w,!u) !l ->
+                                      -- for some j (0 <= j < n)
+                                      --   y = BS.index t j :: Char
+                                      --   w = (arr ! (i+1)) ! j :: Word16
+                                      --   u = (arr ! (i+1)) ! (j+1) :: Word16
+                                      --   l = (arr ! i) ! (j+1) :: Word16
+                                      if x == y
+                                      then u + 1
+                                      else max w l
+                                   ) 0 (U.zip3 t' (U.init v) (U.tail v))
+                        ) (U.replicate (n+1) 0) (V.fromListN m $ BS.unpack s)
+  where !m = BS.length s
+        !n = BS.length t
+        !t' = U.fromListN n (BS.unpack t)
 
 main = do
   s <- BS.getLine
   t <- BS.getLine
   -- BS.length s <= 3000, BS.length t <= 3000, BS.all isAsciiLower s, BS.all isAsciiLower t
-  let !table = lcsTable2 s t
+  let !table = lcsTable s t
       !m = BS.length s
       !n = BS.length t
   let recon !i !j | i >= m || j >= n = Nothing
                   | x == y = let !i' = i+1 ; !j' = j+1
                              in Just (x, (i', j'))
-                  | table ! (i+1,j) >= table ! (i,j+1) = recon (i+1) j
+                  | (table V.! (i+1)) U.! j >= (table V.! i) U.! (j+1) = recon (i+1) j
                   | otherwise = recon i (j+1)
-        where x = BSW.index s i
-              y = BSW.index t j
-      (result, _) = BSW.unfoldrN (fromIntegral $ table ! (0,0)) (\(!i,!j) -> recon i j) (0,0)
+        where x = BS.index s i
+              y = BS.index t j
+      (result, _) = BS.unfoldrN (fromIntegral $ (table V.! 0) U.! 0) (\(!i,!j) -> recon i j) (0,0)
   BS.putStrLn result
