1a13c47f852e56f9f538dc0cc7d67019e0e39fb0
ARATA Mizuki <minorinoki@gmail.com>
2019-05-08 23:12:25 +0900

    AGC033-A: Faster solution (101ms)

--- agc033-a/Main.hs
+++ agc033-a/Main.hs
@@ -1,11 +1,15 @@
 {-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE FlexibleContexts #-}
+-- {-# LANGUAGE PartialTypeSignatures #-}
+-- {-# OPTIONS_GHC -Wno-partial-type-signatures #-}
+import Control.Monad
 import qualified Data.Vector as V
+import qualified Data.Vector.Unboxed as U
+import qualified Data.Vector.Unboxed.Mutable as UM
 import qualified Data.ByteString.Char8 as BS
-import Data.Array.Unboxed
 import Data.Array.ST
 import Control.Monad.ST
-import Control.Monad.State.Strict
+import Data.STRef
 
 asSTUArray :: ST s (STUArray s i a) -> ST s (STUArray s i a)
 asSTUArray = id
@@ -15,29 +19,38 @@ main = do
   -- 1 <= h <= 1000, 1 <= w <= 1000
   initialState <- V.replicateM h BS.getLine
   -- for all (i, j), ((a ! i) `BS.index` j) `elem` "#."
+  let initCells :: U.Vector (Int,Int) -- [(Int,Int)]
+      initCells = U.fromList [(i,j) | i <- [0..h-1], j <- [0..w-1], (initialState V.! i) `BS.index` j == '#']
   let answer :: Int
       answer = runST $ do
         arr <- asSTUArray $ newArray ((0,0),(h-1,w-1)) False
-        let initCells :: [(Int,Int)]
-            initCells = [(i,j) | i <- [0..h-1], j <- [0..w-1], (initialState V.! i) `BS.index` j == '#']
-        forM_ initCells $ \p ->
+        U.forM_ initCells $ \p ->
           writeArray arr p True
-        let -- loop :: Int -> [(Int,Int)] -> m Int
+        let -- loop :: Int -> U.Vector (Int,Int) -> _ Int
             loop !k xs = do
-              ys <- flip execStateT [] $ do
-                let doCell p@(!i,!j) | 0 <= i && i < h && 0 <= j && j < w = do
-                                         b <- lift $ readArray arr p
-                                         when (not b) $ do
-                                           lift $ writeArray arr p True
-                                           modify' (p :)
-                                     | otherwise = return ()
-                forM_ xs $ \(i,j) -> do
-                  doCell (i-1,j)
-                  doCell (i+1,j)
-                  doCell (i,j-1)
-                  doCell (i,j+1)
-              case ys of
-                [] -> return k
-                _ -> loop (k+1) ys
+              lenRef <- UM.replicate 1 0
+              vec <- UM.new (4 * U.length xs)
+              let -- push :: (Int,Int) -> _ ()
+                  push value = do
+                    len <- UM.read lenRef 0
+                    UM.write vec len value
+                    UM.write lenRef 0 (len + 1)
+              let doCell !i !j
+                    | 0 <= i && i < h && 0 <= j && j < w = do
+                        b <- readArray arr (i,j)
+                        when (not b) $ do
+                          writeArray arr (i,j) True
+                          push (i,j)
+                    | otherwise = return ()
+              U.forM_ xs $ \(i,j) -> do
+                doCell (i-1) j
+                doCell (i+1) j
+                doCell i (j-1)
+                doCell i (j+1)
+              len <- UM.read lenRef 0
+              if len == 0
+                then return k
+                else do ys <- U.take len <$> U.unsafeFreeze vec
+                        loop (k+1) ys
         loop 0 initCells
   print answer
