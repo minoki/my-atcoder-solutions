f153a40d3423cd312f32cbcbc79d6186f1e93184
ARATA Mizuki <minorinoki@gmail.com>
2019-05-20 15:03:46 +0900

    ABC126-D: Use `IntMap.IntMap [(Int,Weight)]`

--- abc126-d/Main.hs
+++ abc126-d/Main.hs
@@ -1,7 +1,8 @@
+-- https://github.com/minoki/my-atcoder-solutions
 {-# LANGUAGE BangPatterns #-}
-{-# LANGUAGE ScopedTypeVariables #-}
 import Data.Char
 import Data.List
+import Data.Maybe
 import qualified Data.Vector.Unboxed as U
 import qualified Data.Vector.Unboxed.Mutable as UM
 import qualified Data.ByteString.Char8 as BS
@@ -9,25 +10,28 @@ import qualified Data.IntMap as IntMap
 import Control.Monad
 
 type Weight = Int
+type Tree = IntMap.IntMap [(Int,Weight)]
+
+insert' :: Int -> a -> IntMap.IntMap [a] -> IntMap.IntMap [a]
+insert' !k v = IntMap.alter (\m -> Just (v : fromMaybe [] m)) k
 
 main = do
   n <- readLn
   edges <- U.replicateM (n-1) $ do
     [u,v,w] <- unfoldr (BS.readInt . BS.dropWhile isSpace) <$> BS.getLine
-    return (u, v, w)
-  let tree :: IntMap.IntMap (IntMap.IntMap Weight)
-      tree = U.foldl' (\m (u, v, w) -> IntMap.insertWith IntMap.union (u-1) (IntMap.singleton (v-1) w) $ IntMap.insertWith IntMap.union (v-1) (IntMap.singleton (u-1) w) m) IntMap.empty edges
+    return (u-1, v-1, w)
+  let tree :: Tree
+      tree = U.foldl' (\m (u,v,w) -> insert' u (v,w) $ insert' v (u,w) m) IntMap.empty edges
   let result = U.create $ do
         vec <- UM.replicate n (-1)
         -- white: 0, black: 1
-        let traverse !i !color = do
+        let dfs !i !color = do
               x <- UM.read vec i
-              if x == -1
-                then do UM.write vec i color
-                        let Just t = IntMap.lookup i tree
-                        forM_ (IntMap.toList t) $ \(j,w) -> do
-                          traverse j ((color + w) `rem` 2)
-                else return ()
-        traverse 0 0
+              when (x == -1) $ do
+                UM.write vec i color
+                let Just t = IntMap.lookup i tree
+                forM_ t $ \(j,w) -> do
+                  dfs j ((color + w) `rem` 2)
+        dfs 0 0
         return vec
-  U.forM_ result $ print
+  U.mapM_ print result
