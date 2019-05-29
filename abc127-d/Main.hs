db3ae5bca7d9b1a0abf99d5dcfd50a20c229cf7a
ARATA Mizuki <minorinoki@gmail.com>
2019-05-29 18:34:24 +0900

    ABC127-D: Use merge sort

--- abc127-d/Main.hs
+++ abc127-d/Main.hs
@@ -1,3 +1,4 @@
+-- https://github.com/minoki/my-atcoder-solutions
 {-# LANGUAGE BangPatterns #-}
 import Data.List
 import Data.Char
@@ -5,6 +6,33 @@ import Data.Monoid
 import Control.Monad
 import qualified Data.ByteString.Char8 as BS
 import qualified Data.Vector.Unboxed as U
+import qualified Data.Vector.Unboxed.Mutable as UM
+
+mergeSortBy :: (U.Unbox a) => (a -> a -> Ordering) -> U.Vector a -> U.Vector a
+mergeSortBy !cmp !vec = doSort vec
+  where
+    doSort vec | U.length vec <= 1 = vec
+               | otherwise = let (xs, ys) = U.splitAt (U.length vec `quot` 2) vec
+                             in merge (doSort xs) (doSort ys)
+    merge xs ys = U.create $ do
+      let !n = U.length xs
+          !m = U.length ys
+      result <- UM.new (n + m)
+      let loop !i !j
+            | i == n = U.copy (UM.drop (i + j) result) (U.drop j ys)
+            | j == m = U.copy (UM.drop (i + j) result) (U.drop i xs)
+            | otherwise = let !x = xs U.! i
+                              !y = ys U.! j
+                          in case cmp x y of
+                               LT -> do UM.write result (i + j) x
+                                        loop (i + 1) j
+                               EQ -> do UM.write result (i + j) x
+                                        UM.write result (i + j + 1) y
+                                        loop (i + 1) (j + 1)
+                               GT -> do UM.write result (i + j) y
+                                        loop i (j + 1)
+      loop 0 0
+      return result
 
 spanWithLimit :: (U.Unbox a) => Int -> (a -> Bool) -> U.Vector a -> (U.Vector a, U.Vector a)
 spanWithLimit !limit !f !vec | U.length vec <= limit = U.span f vec
@@ -15,9 +43,9 @@ spanWithLimit !limit !f !vec | U.length vec <= limit = U.span f vec
 main = do
   [n,m] <- unfoldr (BS.readInt . BS.dropWhile isSpace) <$> BS.getLine
   -- as: a が小さい順にソートする
-  as <- U.fromListN n . sort . unfoldr (BS.readInt . BS.dropWhile isSpace) <$> BS.getLine
+  as <- mergeSortBy compare . U.unfoldrN n (BS.readInt . BS.dropWhile isSpace) <$> BS.getLine
   -- bc: c が大きい順にソートする
-  bc <- fmap (sortBy (\(b,c) (b',c') -> compare c' c <> compare b b')) $ replicateM m $ do
+  bc <- fmap (U.toList . mergeSortBy (\(b,c) (b',c') -> compare c' c <> compare b b')) $ U.replicateM m $ do
     [b,c] <- unfoldr (BS.readInt . BS.dropWhile isSpace) <$> BS.getLine
     return (b,c)
   let f !s !cards [] = s + U.sum cards
