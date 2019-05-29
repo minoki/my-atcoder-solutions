9b54ae82b33de43370c74a150ccc325d0f094f29
ARATA Mizuki <minorinoki@gmail.com>
2019-05-29 18:34:41 +0900

    ABC127-C: Use readIntPair

--- abc127-c/Main.hs
+++ abc127-c/Main.hs
@@ -1,14 +1,18 @@
+-- https://github.com/minoki/my-atcoder-solutions
 {-# LANGUAGE BangPatterns #-}
 import Data.List
 import Data.Char
 import qualified Data.ByteString.Char8 as BS
 import qualified Data.Vector.Unboxed as U
 
+readIntPair :: BS.ByteString -> (Int, Int)
+readIntPair s = let Just (a, s') = BS.readInt s
+                    Just (b, _) = BS.readInt $ BS.dropWhile isSpace s'
+                in (a, b)
+
 main = do
-  [n,m] <- unfoldr (BS.readInt . BS.dropWhile isSpace) <$> BS.getLine
-  (ls,rs) <- fmap U.unzip $ U.replicateM m $ do
-    [l,r] <- unfoldr (BS.readInt . BS.dropWhile isSpace) <$> BS.getLine
-    return (l,r)
+  (n,m) <- readIntPair <$> BS.getLine
+  (ls,rs) <- fmap U.unzip $ U.replicateM m (readIntPair <$> BS.getLine)
   let maxL = U.maximum ls
       minR = U.minimum rs
   print $ max 0 (minR - maxL + 1)
