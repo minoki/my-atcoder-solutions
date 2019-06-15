1cede9b6db8717b2552524494ed2c4922cab28c9
ARATA Mizuki <minorinoki@gmail.com>
2019-06-16 00:47:04 +0900

    diverta2019-2 - C: Use merge sort

--- diverta2019-2-c/Main.hs
+++ diverta2019-2-c/Main.hs
@@ -4,12 +4,14 @@ import Data.Char (isSpace)
 import Data.List (unfoldr, sort)
 import Control.Monad (forM_)
 import qualified Data.Vector.Unboxed as U
+import qualified Data.Vector.Unboxed.Mutable as UM
 import qualified Data.ByteString.Char8 as BS
 
 main = do
   n <- readLn
   -- n >= 2
-  xs <- U.fromListN n . sort . unfoldr (BS.readInt . BS.dropWhile isSpace) <$> BS.getLine
+  -- xs <- U.fromListN n . sort . unfoldr (BS.readInt . BS.dropWhile isSpace) <$> BS.getLine
+  xs <- mergeSortBy compare . U.unfoldrN n (BS.readInt . BS.dropWhile isSpace) <$> BS.getLine
   let (xs0, xs1) = U.span (< 0) xs
   let (r, zs)
         | U.length xs0 == 0 =
@@ -41,3 +43,31 @@ main = do
   print r
   forM_ zs $ \(x,y) ->
     putStrLn $ concat [show x, " ", show y]
+
+---
+
+mergeSortBy :: (U.Unbox a) => (a -> a -> Ordering) -> U.Vector a -> U.Vector a
+mergeSortBy !cmp !vec = doSort vec
+  where
+    doSort vec | U.length vec <= 1 = vec
+               | otherwise = let (xs, ys) = U.splitAt (U.length vec `quot` 2) vec
+                             in merge (doSort xs) (doSort ys)
+    merge xs ys = U.create $ do
+      let !n = U.length xs
+          !m = U.length ys
+      result <- UM.new (n + m)
+      let loop !i !j
+            | i == n = U.copy (UM.drop (i + j) result) (U.drop j ys)
+            | j == m = U.copy (UM.drop (i + j) result) (U.drop i xs)
+            | otherwise = let !x = xs U.! i
+                              !y = ys U.! j
+                          in case cmp x y of
+                               LT -> do UM.write result (i + j) x
+                                        loop (i + 1) j
+                               EQ -> do UM.write result (i + j) x
+                                        UM.write result (i + j + 1) y
+                                        loop (i + 1) (j + 1)
+                               GT -> do UM.write result (i + j) y
+                                        loop i (j + 1)
+      loop 0 0
+      return result
