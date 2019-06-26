e40e9d72089bac740aa9c2ec78ca1c3295435260
ARATA Mizuki <minorinoki@gmail.com>
2019-06-27 02:47:15 +0900

    Exawizards2019-D: Some improvement (still slow)

--- exawizards2019-d/Main.hs
+++ exawizards2019-d/Main.hs
@@ -1,31 +1,34 @@
 -- https://github.com/minoki/my-atcoder-solutions
 {-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
 import Control.Monad
 import Data.Char (isSpace)
-import Data.Int
+import Data.Int (Int64)
 import Data.List
 import qualified Data.Vector.Unboxed as U
 import qualified Data.Vector.Unboxed.Mutable as UM
 import qualified Data.ByteString.Char8 as BS
--- import Debug.Trace
-
-newtype N = N Int64 deriving (Eq, Show)
-modulo = 10^9+7 :: Int64
-instance Num N where
-  N x + N y = N ((x + y) `rem` modulo)
-  N x - N y = N ((x - y) `mod` modulo)
-  N x * N y = N ((x * y) `rem` modulo)
-  fromInteger n = N (fromInteger (n `mod` fromIntegral modulo))
-  abs = undefined; signum = undefined
+--
+import Data.Coerce
+import qualified Data.Vector.Generic
+import qualified Data.Vector.Generic.Mutable
+import Debug.Trace
 
 solve :: Int -> Int -> [Int] -> N -> N
 solve !x 0 [] !c = c * fromIntegral x
 -- solve !x 1 [y] c bc = c * (traceShow (c,bc,x) $ fromIntegral (x `rem` y))
-solve !x !n ss !c = sum $ do
-  (k, t:ts) <- zip [0..] $ tails ss
-  -- k + length ts + 1 == n
-  -- k : t より大きいやつ
-  return $ solve (x `rem` t) (n - k - 1) ts (product (map fromIntegral [n-1,n-2..n-k]) * c)
+solve !x !n ss !c =
+  let (ss0,ss1) = span (> x) ss
+  in if null ss1
+     then factV U.! n * c * fromIntegral x
+     else let !m = length ss0
+              !q = factV U.! n / factV U.! (n-m)
+          in sum $ do (k, t:ts) <- zip [m..] $ tails ss1
+                      -- k + length ts + 1 == n
+                      -- k : t より大きいやつ
+                      let !p = factV U.! (n-m-1) / factV U.! (n-k-1)
+                      return $ solve (x `rem` t) (n - k - 1) ts (q * p * c)
 -- n == length ss
 
 main = do
@@ -33,8 +36,57 @@ main = do
   -- n <= 200, x <= 10^5
   ss <- U.toList . mergeSortBy (\x y -> compare y x) . U.unfoldrN n (BS.readInt . BS.dropWhile isSpace) <$> BS.getLine
   -- si <= 10^5
-  let N result = solve x n ss 1
-  print result
+  print $ solve x n ss 1
+
+factV :: U.Vector N
+factV = U.scanl' (*) 1 (U.enumFromN 1 200)
+
+---
+
+modulo :: Int64
+modulo = 10^9+7
+addMod, subMod, mulMod :: Int64 -> Int64 -> Int64
+addMod !x !y | x + y >= modulo = x + y - modulo
+             | otherwise = x + y
+subMod !x !y | x >= y = x - y
+             | otherwise = x - y + modulo
+mulMod !x !y = (x * y) `rem` modulo
+
+newtype N = N { unwrapN :: Int64 } deriving (Eq)
+instance Show N where
+  show (N x) = show x
+instance Num N where
+  (+) = coerce addMod
+  (-) = coerce subMod
+  (*) = coerce mulMod
+  fromInteger n = N (fromInteger (n `mod` fromIntegral modulo))
+  abs = undefined; signum = undefined
+
+{-# RULES
+"^9/Int" forall x. x ^ (9 :: Int) = let u = x; v = u * u * u in v * v * v
+"^9/Integer" forall x. x ^ (9 :: Integer) = let u = x; v = u * u * u in v * v * v
+ #-}
+
+---
+
+exEuclid :: (Eq a, Integral a) => a -> a -> (a, a, a)
+exEuclid !f !g = loop 1 0 0 1 f g
+  where loop !u0 !u1 !v0 !v1 !f 0 = (f, u0, v0)
+        loop !u0 !u1 !v0 !v1 !f g =
+          case divMod f g of
+            (q,r) -> loop u1 (u0 - q * u1) v1 (v0 - q * v1) g r
+
+recipM :: Int64 -> Int64
+recipM !x = case exEuclid x modulo of
+             (1,a,_) -> a `mod` modulo
+             (-1,a,_) -> (-a) `mod` modulo
+divM :: Int64 -> Int64 -> Int64
+divM !x !y = x `mulMod` recipM y
+
+instance Fractional N where
+  (/) = coerce divM
+  recip = coerce recipM
+  fromRational = undefined
 
 ---
 
@@ -63,3 +115,34 @@ mergeSortBy !cmp !vec = doSort vec
                                         loop i (j + 1)
       loop 0 0
       return result
+
+---
+
+newtype instance UM.MVector s N = MV_N (UM.MVector s Int64)
+newtype instance U.Vector N = V_N (U.Vector Int64)
+
+instance Data.Vector.Generic.Mutable.MVector UM.MVector N where -- needs MultiParamTypeClasses here
+  basicLength (MV_N mv) = Data.Vector.Generic.Mutable.basicLength mv
+  basicUnsafeSlice i l (MV_N mv) = MV_N (Data.Vector.Generic.Mutable.basicUnsafeSlice i l mv)
+  basicOverlaps (MV_N mv) (MV_N mv') = Data.Vector.Generic.Mutable.basicOverlaps mv mv'
+  basicUnsafeNew l = MV_N <$> Data.Vector.Generic.Mutable.basicUnsafeNew l
+  basicInitialize (MV_N mv) = Data.Vector.Generic.Mutable.basicInitialize mv
+  basicUnsafeReplicate i x = MV_N <$> Data.Vector.Generic.Mutable.basicUnsafeReplicate i (coerce x)
+  basicUnsafeRead (MV_N mv) i = coerce <$> Data.Vector.Generic.Mutable.basicUnsafeRead mv i
+  basicUnsafeWrite (MV_N mv) i x = Data.Vector.Generic.Mutable.basicUnsafeWrite mv i (coerce x)
+  basicClear (MV_N mv) = Data.Vector.Generic.Mutable.basicClear mv
+  basicSet (MV_N mv) x = Data.Vector.Generic.Mutable.basicSet mv (coerce x)
+  basicUnsafeCopy (MV_N mv) (MV_N mv') = Data.Vector.Generic.Mutable.basicUnsafeCopy mv mv'
+  basicUnsafeMove (MV_N mv) (MV_N mv') = Data.Vector.Generic.Mutable.basicUnsafeMove mv mv'
+  basicUnsafeGrow (MV_N mv) n = MV_N <$> Data.Vector.Generic.Mutable.basicUnsafeGrow mv n
+
+instance Data.Vector.Generic.Vector U.Vector N where -- needs MultiParamTypeClasses here
+  basicUnsafeFreeze (MV_N mv) = V_N <$> Data.Vector.Generic.basicUnsafeFreeze mv
+  basicUnsafeThaw (V_N v) = MV_N <$> Data.Vector.Generic.basicUnsafeThaw v
+  basicLength (V_N v) = Data.Vector.Generic.basicLength v
+  basicUnsafeSlice i l (V_N v) = V_N (Data.Vector.Generic.basicUnsafeSlice i l v)
+  basicUnsafeIndexM (V_N v) i = coerce <$> Data.Vector.Generic.basicUnsafeIndexM v i
+  basicUnsafeCopy (MV_N mv) (V_N v) = Data.Vector.Generic.basicUnsafeCopy mv v
+  elemseq (V_N v) x y = Data.Vector.Generic.elemseq v (coerce x) y
+
+instance U.Unbox N
