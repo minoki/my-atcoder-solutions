2806de557d93d30c5f41f94928b911e6e552a814
ARATA Mizuki <minorinoki@gmail.com>
2019-06-27 01:27:39 +0900

    Exawizards2019-D: Use merge sort (still slow)

--- exawizards2019-d/Main.hs
+++ exawizards2019-d/Main.hs
@@ -1,11 +1,13 @@
+-- https://github.com/minoki/my-atcoder-solutions
 {-# LANGUAGE BangPatterns #-}
 import Control.Monad
+import Data.Char (isSpace)
 import Data.Int
 import Data.List
-import qualified Data.Vector.Unboxed as V
-import qualified Data.Vector.Unboxed.Mutable as VM
+import qualified Data.Vector.Unboxed as U
+import qualified Data.Vector.Unboxed.Mutable as UM
 import qualified Data.ByteString.Char8 as BS
-import Debug.Trace
+-- import Debug.Trace
 
 newtype N = N Int64 deriving (Eq, Show)
 modulo = 10^9+7 :: Int64
@@ -27,9 +29,37 @@ solve !x !n ss !c = sum $ do
 -- n == length ss
 
 main = do
-  [n,x] <- map (read . BS.unpack) . BS.words <$> BS.getLine
+  [n,x] <- unfoldr (BS.readInt . BS.dropWhile isSpace) <$> BS.getLine
   -- n <= 200, x <= 10^5
-  ss <- {- V.fromListN n . -} sortBy (\x y -> compare y x) . map (read . BS.unpack) . BS.words <$> BS.getLine
+  ss <- U.toList . mergeSortBy (\x y -> compare y x) . U.unfoldrN n (BS.readInt . BS.dropWhile isSpace) <$> BS.getLine
   -- si <= 10^5
   let N result = solve x n ss 1
   print result
+
+---
+
+mergeSortBy :: (U.Unbox a) => (a -> a -> Ordering) -> U.Vector a -> U.Vector a
+mergeSortBy !cmp !vec = doSort vec
+  where
+    doSort vec | U.length vec <= 1 = vec
+               | otherwise = let (xs, ys) = U.splitAt (U.length vec `quot` 2) vec
+                             in merge (doSort xs) (doSort ys)
+    merge xs ys = U.create $ do
+      let !n = U.length xs
+          !m = U.length ys
+      result <- UM.new (n + m)
+      let loop !i !j
+            | i == n = U.copy (UM.drop (i + j) result) (U.drop j ys)
+            | j == m = U.copy (UM.drop (i + j) result) (U.drop i xs)
+            | otherwise = let !x = xs U.! i
+                              !y = ys U.! j
+                          in case cmp x y of
+                               LT -> do UM.write result (i + j) x
+                                        loop (i + 1) j
+                               EQ -> do UM.write result (i + j) x
+                                        UM.write result (i + j + 1) y
+                                        loop (i + 1) (j + 1)
+                               GT -> do UM.write result (i + j) y
+                                        loop i (j + 1)
+      loop 0 0
+      return result
