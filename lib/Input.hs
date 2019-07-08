1a1287c584f50ae55056a332153833b3b269a947
ARATA Mizuki <minorinoki@gmail.com>
2019-07-08 19:29:07 +0900

    Library: Add readInt64

--- lib/Input.hs
+++ lib/Input.hs
@@ -5,6 +5,7 @@ import Data.Char (isSpace)
 import qualified Data.Vector.Unboxed as U
 import qualified Data.Vector.Unboxed.Mutable as UM
 import qualified Data.ByteString.Char8 as BS
+import Data.Bifunctor (first)
 
 main = do
   _ :: [Int] <- map (read . BS.unpack) . BS.words <$> BS.getLine
@@ -13,3 +14,6 @@ main = do
     [x,y,z] <- unfoldr (BS.readInt . BS.dropWhile isSpace) <$> BS.getLine
     return (x-1,y-1,z)
   return ()
+
+readInt64 :: BS.ByteString -> Maybe (Int64, BS.ByteString)
+readInt64 s = first fromIntegral <$> BS.readInt s
