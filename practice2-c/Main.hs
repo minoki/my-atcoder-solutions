ff66bc00a32ca85f24ca5409a3da543cb6bb756b
ARATA Mizuki <minorinoki@gmail.com>
2020-09-11 09:35:13 +0900

    More clean up

--- practice2-c/Main.hs
+++ practice2-c/Main.hs
@@ -1,5 +1,7 @@
 -- https://github.com/minoki/my-atcoder-solutions
+{-# LANGUAGE BangPatterns     #-}
 {-# LANGUAGE TypeApplications #-}
+import           Control.Exception     (assert)
 import           Control.Monad
 import           Data.Bits
 import qualified Data.ByteString.Char8 as BS
@@ -8,7 +10,6 @@ import           Data.Int              (Int64)
 import           Data.List             (unfoldr)
 import           Data.Ratio
 import qualified Test.QuickCheck       as QC
-import Control.Exception (assert)
 
 -- comb2 n = n * (n - 1) `quot` 2 without undue overflow
 -- n even: comb2 n = (n `quot` 2) * (n - 1)
@@ -17,32 +18,43 @@ comb2 :: (Integral a, Bits a) => a -> a
 comb2 n = (n `shiftR` 1) * ((n - 1) .|. 1)
 
 -- floorSum n m a b
--- n: non-negative, m: positive
+-- Assumptions:
+-- * n: non-negative, m: positive
 floorSum :: Int64 -> Int64 -> Int64 -> Int64 -> Int64
-floorSum n m a b | assert (n >= 0 && m > 0) False = undefined
-floorSum n m 0 b = n * floor (b % m)
-floorSum 0 m a b = 0
-floorSum n 1 a b = a * comb2 n + n * b
-floorSum n m a b
+floorSum !n !m !a !b
+  | assert (n >= 0 && m > 0) False = undefined
   | a < 0 = floorSum n m (- a) (b + a * (n - 1))
-{-
-  | a >= m || a < 0 = case a `divMod` m of
-                        (q, a') -> q * comb2 n + floorSum n m a' b
+  {-
+  | m < n = case n `quotRem` m of
+              (q, n') -> (q * n - comb2 (q + 1) * m) * a +
+                         q * floorSum_positive 0 m m a b +
+                         floorSum_positive 0 n' m a b
 -}
-  | let m2 = m `quot` 2
-  , abs a > m2 = case (a + m2) `divMod` m of
-                   (q, a') -> q * comb2 n + floorSum n m (a' - m2) b
-  | b >= m || b < 0 = case b `divMod` m of
-                        (q, b') -> q * n + floorSum n m a b'
-  | n > m = case n `quotRem` m of
-              (q, n') -> (q * n - comb2 (q + 1) * m) * a + q * floorSum m m a b + floorSum n' m a b
-  | otherwise =
-      -- 0 < a < m, 0 <= b < m, 0 < n <= m
-      let t = floor ((toInteger a * (toInteger n - 1) + toInteger b) % toInteger m)
-      in n * t + floorSum t a (- m) (b - m)
+  | otherwise = floorSum_positive 0 n m a b
+  where
+    -- Invariants:
+    -- * n: non-negative, m: positive, a: non-negative
+    -- * 0 <= n <= m
+    floorSum_positive :: Int64 -> Int64 -> Int64 -> Int64 -> Int64 -> Int64
+    floorSum_positive !acc !n !m !a !b
+      | a == 0 = acc + n * (b `div` m)
+      | n == 0 = acc
+      | m == 1 = acc + a * comb2 n + b * n
+      | let m2 = m `quot` 2, a > m2 =
+              let (q, a') = (a + m2) `quotRem` m
+                  (a'',b'') = if a' < m2 then
+                                (m2 - a', b - (m2 - a') * (n - 1))
+                              else
+                                (a' - m2, b)
+              in floorSum_positive (acc + q * comb2 n) n m a'' b''
+      | otherwise =
+          let (q, b') = b `divMod` m
+              t = (a * (n - 1) + b') `div` m
+              -- t <= (m * (m - 1) + m) `div` m = m
+          in floorSum_positive (acc + n * (q + t)) t a m (b' - m * t)
 
 floorSum_naive :: Int64 -> Int64 -> Int64 -> Int64 -> Integer
-floorSum_naive n m a b = sum [ floor ((fromIntegral a * fromIntegral i + fromIntegral b) % fromIntegral m) | i <- [0..n-1] ]
+floorSum_naive n m a b = sum [ floor ((toInteger a * toInteger i + toInteger b) % toInteger m) | i <- [0..n-1] ]
 
 main = do
   t <- readLn @Int
@@ -61,5 +73,4 @@ prop_floorSum_r = QC.forAllShrink (QC.choose (1, 10^4)) QC.shrink $ \n -> n >= 1
   QC.forAllShrink (QC.choose (1, 10^9)) QC.shrink $ \m -> m >= 1 QC.==>
   QC.forAllShrink (QC.choose (0, m - 1)) QC.shrink $ \a ->
   QC.forAllShrink (QC.choose (0, m - 1)) QC.shrink $ \b ->
-  QC.within (100 * 1000) $ toInteger (floorSum n m a b) QC.=== floorSum_naive n m a b
-
+  QC.within (100 * 1000 {- 100ms -}) $ toInteger (floorSum n m a b) QC.=== floorSum_naive n m a b
