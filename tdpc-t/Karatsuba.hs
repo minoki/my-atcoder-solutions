bf187ef3cb3837eddeafcfd855180663e543f9d2
ARATA Mizuki <minorinoki@gmail.com>
2021-01-18 16:55:22 +0900

    TDPC-T: Karatsuba

---
+++ tdpc-t/Karatsuba.hs
@@ -0,0 +1,280 @@
+-- https://github.com/minoki/my-atcoder-solutions
+{-# LANGUAGE BangPatterns        #-}
+{-# LANGUAGE DataKinds           #-}
+{-# LANGUAGE NoStarIsType        #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeFamilies        #-}
+{-# LANGUAGE TypeOperators       #-}
+import           Control.Monad
+import           Control.Monad.ST
+import           Data.Bits
+import qualified Data.ByteString.Char8        as BS
+import           Data.Char                    (isSpace)
+import           Data.Coerce
+import           Data.Int                     (Int64)
+import           Data.List                    (foldl', tails, unfoldr)
+import qualified Data.Vector.Generic          as G
+import qualified Data.Vector.Unboxing         as U
+import qualified Data.Vector.Unboxing.Mutable as UM
+import           GHC.TypeNats                 (type (+), KnownNat, Nat,
+                                               type (^), natVal)
+
+type P = U.Vector (IntMod (10^9 + 7))
+type PM s = UM.MVector s (IntMod (10^9 + 7))
+
+{-
+sum' :: KnownNat m => [IntMod m] -> IntMod m
+sum' = fromIntegral . foldl' (\x y -> x + unwrapN y) 0
+{-# INLINE sum' #-}
+-}
+
+-- 多項式は
+--   U.fromList [a,b,c,...,z] = a + b * X + c * X^2 + ... + z * X^(k-1)
+-- により表す。
+
+-- 多項式を X^k - X^(k-1) - ... - X - 1 で割った余りを返す。
+reduceM :: Int -> PM s -> ST s (PM s)
+reduceM !k !v = loop (UM.length v)
+  where loop !l | l <= k = return (UM.take l v)
+                | otherwise = do b <- UM.read v (l - 1)
+                                 forM_ [l - k - 1 .. l - 2] $ \i -> do
+                                   UM.modify v (+ b) i
+                                 loop (l - 1)
+
+-- 多項式の積を X^k - X^(k-1) - ... - X - 1 で割った余りを返す。
+mulP :: Int -> P -> P -> P
+mulP !k !v !w = {- U.force $ -} U.create $ do
+  let !vl = U.length v
+      !wl = U.length w
+  -- s <- UM.new (vl + wl - 1)
+  -- forM_ [0 .. vl + wl - 2] $ \i -> do
+  --   let !x = sum' [(v U.! (i-j)) * (w U.! j) | j <- [max 0 (i - vl + 1) .. min (wl - 1) i]]
+  --   UM.write s i x
+  let n = ceiling ((log (fromIntegral (vl .|. wl)) :: Double) / log 2) :: Int
+  s <- U.thaw (doMulP (2^n) v w)
+  reduceM k s
+
+-- 多項式に X をかけたものを X^k - X^(k-1) - ... - X - 1 で割った余りを返す。
+mulByX :: Int -> P -> P
+mulByX !k !v
+  | U.length v == k = let !v_k = v U.! (k-1)
+                      in U.generate k $ \i -> if i == 0 then
+                                                v_k
+                                              else
+                                                v_k + (v U.! (i - 1))
+  | otherwise = U.cons 0 v
+
+-- X の（mod X^k - X^(k-1) - ... - X - 1 での）n 乗
+powX :: Int -> Int -> P
+powX !k !n = doPowX n
+  where
+    doPowX 0 = U.fromList [1]   -- 1
+    doPowX 1 = U.fromList [0,1] -- X
+    doPowX i = case i `quotRem` 2 of
+                 (j,0) -> let !f = doPowX j -- X^j mod P
+                          in mulP k f f
+                 (j,_) -> let !f = doPowX j -- X^j mod P
+                          in mulByX k (mulP k f f)
+
+main :: IO ()
+main = do
+  [k,n] <- unfoldr (BS.readInt . BS.dropWhile isSpace) <$> BS.getLine
+  -- 2 <= k <= 1000
+  -- 1 <= n <= 10^9
+  if n <= k then
+    print 1
+  else do
+    let f = powX k (n - k) -- X^(n-k) mod X^k - X^(k-1) - ... - X - 1
+    let seq = replicate k 1 ++ map (sum . take k) (tails seq) -- 数列
+    print $ sum $ zipWith (*) (U.toList f) (drop (k-1) seq)
+
+--
+-- Modular Arithmetic
+--
+
+newtype IntMod (m :: Nat) = IntMod { unwrapN :: Int64 } deriving (Eq)
+
+instance Show (IntMod m) where
+  show (IntMod x) = show x
+
+instance KnownNat m => Num (IntMod m) where
+  t@(IntMod x) + IntMod y
+    | x + y >= modulus = IntMod (x + y - modulus)
+    | otherwise = IntMod (x + y)
+    where modulus = fromIntegral (natVal t)
+  t@(IntMod x) - IntMod y
+    | x >= y = IntMod (x - y)
+    | otherwise = IntMod (x - y + modulus)
+    where modulus = fromIntegral (natVal t)
+  t@(IntMod x) * IntMod y = IntMod ((x * y) `rem` modulus)
+    where modulus = fromIntegral (natVal t)
+  fromInteger n = let result = IntMod (fromInteger (n `mod` fromIntegral modulus))
+                      modulus = natVal result
+                  in result
+  abs = undefined; signum = undefined
+  {-# SPECIALIZE instance Num (IntMod 1000000007) #-}
+
+fromIntegral_Int64_IntMod :: KnownNat m => Int64 -> IntMod m
+fromIntegral_Int64_IntMod n = result
+  where
+    result | 0 <= n && n < modulus = IntMod n
+           | otherwise = IntMod (n `mod` modulus)
+    modulus = fromIntegral (natVal result)
+
+{-# RULES
+"fromIntegral/Int->IntMod" fromIntegral = fromIntegral_Int64_IntMod . (fromIntegral :: Int -> Int64) :: Int -> IntMod (10^9 + 7)
+"fromIntegral/Int64->IntMod" fromIntegral = fromIntegral_Int64_IntMod :: Int64 -> IntMod (10^9 + 7)
+ #-}
+
+instance U.Unboxable (IntMod m) where
+  type Rep (IntMod m) = Int64
+
+--
+-- Univariate polynomial
+--
+
+newtype Poly vec a = Poly { coeffAsc :: vec a } deriving Eq
+
+normalizePoly :: (Eq a, Num a, G.Vector vec a) => vec a -> vec a
+normalizePoly v | G.null v || G.last v /= 0 = v
+                | otherwise = normalizePoly (G.init v)
+
+addPoly :: (Eq a, Num a, G.Vector vec a) => vec a -> vec a -> vec a
+addPoly v w = case compare n m of
+                LT -> G.generate m $ \i -> if i < n
+                                           then v G.! i + w G.! i
+                                           else w G.! i
+                GT -> G.generate n $ \i -> if i < m
+                                           then v G.! i + w G.! i
+                                           else v G.! i
+                EQ -> normalizePoly $ G.zipWith (+) v w
+  where n = G.length v
+        m = G.length w
+
+subPoly :: (Eq a, Num a, G.Vector vec a) => vec a -> vec a -> vec a
+subPoly v w = case compare n m of
+                LT -> G.generate m $ \i -> if i < n
+                                           then v G.! i - w G.! i
+                                           else negate (w G.! i)
+                GT -> G.generate n $ \i -> if i < m
+                                           then v G.! i - w G.! i
+                                           else v G.! i
+                EQ -> normalizePoly $ G.zipWith (-) v w
+  where n = G.length v
+        m = G.length w
+
+naiveMulPoly :: (Num a, G.Vector vec a) => vec a -> vec a -> vec a
+naiveMulPoly v w = G.generate (n + m - 1) $
+                   \i -> sum [(v G.! (i-j)) * (w G.! j) | j <- [max (i-n+1) 0..min i (m-1)]]
+  where n = G.length v
+        m = G.length w
+
+doMulP :: (Eq a, Num a, G.Vector vec a) => Int -> vec a -> vec a -> vec a
+doMulP n !v !w | n <= 16 = naiveMulPoly v w
+doMulP n !v !w
+  | G.null v = v
+  | G.null w = w
+  | G.length v < n2 = let (w0, w1) = G.splitAt n2 w
+                          u0 = doMulP n2 v w0
+                          u1 = doMulP n2 v w1
+                      in G.generate (G.length v + G.length w - 1)
+                         $ \i -> case () of
+                                   _ | i < n2     ->  u0 `at` i
+                                     | i < n      -> (u0 `at` i) + (u1 `at` (i - n2))
+                                     | i < n + n2 -> (u1 `at` (i - n2))
+  | G.length w < n2 = let (v0, v1) = G.splitAt n2 v
+                          u0 = doMulP n2 v0 w
+                          u1 = doMulP n2 v1 w
+                      in G.generate (G.length v + G.length w - 1)
+                         $ \i -> case () of
+                                   _ | i < n2     ->  u0 `at` i
+                                     | i < n      -> (u0 `at` i) + (u1 `at` (i - n2))
+                                     | i < n + n2 -> (u1 `at` (i - n2))
+  | otherwise = let (v0, v1) = G.splitAt n2 v
+                    (w0, w1) = G.splitAt n2 w
+                    v0_1 = v0 `addPoly` v1
+                    w0_1 = w0 `addPoly` w1
+                    p = doMulP n2 v0_1 w0_1
+                    q = doMulP n2 v0 w0
+                    r = doMulP n2 v1 w1
+                    -- s = (p `subPoly` q) `subPoly` r -- p - q - r
+                    -- q + s*X^n2 + r*X^n
+                in G.generate (G.length v + G.length w - 1)
+                   $ \i -> case () of
+                             _ | i < n2     ->   q `at` i
+                               | i < n      -> ((q `at` i) + (p `at` (i - n2))) - ((q `at` (i - n2)) + (r `at` (i - n2)))
+                               | i < n + n2 -> ((r `at` (i - n)) + (p `at` (i - n2))) - ((q `at` (i - n2)) + (r `at` (i - n2)))
+                               | otherwise  ->   r `at` (i - n)
+  where n2 = n `quot` 2
+        at :: (Num a, G.Vector vec a) => vec a -> Int -> a
+        at v i = if i < G.length v then v G.! i else 0
+{-# INLINE doMulP #-}
+
+mulPoly :: (Eq a, Num a, G.Vector vec a) => vec a -> vec a -> vec a
+mulPoly !v !w = let k = ceiling ((log (fromIntegral (max n m)) :: Double) / log 2) :: Int
+                in doMulP (2^k) v w
+  where n = G.length v
+        m = G.length w
+{-# INLINE mulPoly #-}
+
+zeroPoly :: (G.Vector vec a) => Poly vec a
+zeroPoly = Poly G.empty
+
+constPoly :: (Eq a, Num a, G.Vector vec a) => a -> Poly vec a
+constPoly 0 = Poly G.empty
+constPoly x = Poly (G.singleton x)
+
+scalePoly :: (Eq a, Num a, G.Vector vec a) => a -> Poly vec a -> Poly vec a
+scalePoly a (Poly xs)
+  | a == 0 = zeroPoly
+  | otherwise = Poly $ G.map (* a) xs
+
+valueAtPoly :: (Num a, G.Vector vec a) => Poly vec a -> a -> a
+valueAtPoly (Poly xs) t = G.foldr' (\a b -> a + t * b) 0 xs
+
+instance (Eq a, Num a, G.Vector vec a) => Num (Poly vec a) where
+  (+) = coerce (addPoly :: vec a -> vec a -> vec a)
+  (-) = coerce (subPoly :: vec a -> vec a -> vec a)
+  negate (Poly v) = Poly (G.map negate v)
+  (*) = coerce (mulPoly :: vec a -> vec a -> vec a)
+  fromInteger = constPoly . fromInteger
+  abs = undefined; signum = undefined
+
+divModPoly :: (Eq a, Fractional a, G.Vector vec a) => Poly vec a -> Poly vec a -> (Poly vec a, Poly vec a)
+divModPoly f g@(Poly w)
+  | G.null w = error "divModPoly: divide by zero"
+  | degree f < degree g = (zeroPoly, f)
+  | otherwise = loop zeroPoly (scalePoly (recip b) f)
+  where
+    g' = toMonic g
+    b = leadingCoefficient g
+    -- invariant: f == q * g + scalePoly b p
+    loop q p | degree p < degree g = (q, scalePoly b p)
+             | otherwise = let q' = Poly (G.drop (degree' g) (coeffAsc p))
+                           in loop (q + q') (p - q' * g')
+
+    toMonic :: (Fractional a, G.Vector vec a) => Poly vec a -> Poly vec a
+    toMonic f@(Poly xs)
+      | G.null xs = zeroPoly
+      | otherwise = Poly $ G.map (* recip (leadingCoefficient f)) xs
+
+    leadingCoefficient :: (Num a, G.Vector vec a) => Poly vec a -> a
+    leadingCoefficient (Poly xs)
+      | G.null xs = 0
+      | otherwise = G.last xs
+
+    degree :: G.Vector vec a => Poly vec a -> Maybe Int
+    degree (Poly xs) = case G.length xs - 1 of
+      -1 -> Nothing
+      n  -> Just n
+
+    degree' :: G.Vector vec a => Poly vec a -> Int
+    degree' (Poly xs) = case G.length xs of
+      0 -> error "degree': zero polynomial"
+      n -> n - 1
+
+-- 組立除法
+-- second constPoly (divModByDeg1 f t) = divMod f (Poly (G.fromList [-t, 1]))
+divModByDeg1 :: (Eq a, Num a, G.Vector vec a) => Poly vec a -> a -> (Poly vec a, a)
+divModByDeg1 f t = let w = G.postscanr (\a b -> a + b * t) 0 $ coeffAsc f
+                   in (Poly (G.tail w), G.head w)
